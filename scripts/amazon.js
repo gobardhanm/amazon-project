7609c2b84022c6279cd47e6a4b631eb98647a3c7
gobardhanm <gobardhan.meh3r@gmail.com>
2025-09-03 17:40:28 +0530

    deleted some already done stuff and starting from scratch by myself

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,33 @@
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+  },
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+  },
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+  }
+];
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -32,7 +62,7 @@ products.forEach((product) => {
           <option value="4">4</option>
           <option value="5">5</option>
           <option value="6">6</option>
-          <option value="7">7</option>
+          <option value="7">.toFixed(2)7</option>
           <option value="8">8</option>
           <option value="9">9</option>
           <option value="10">10</option>
@@ -55,36 +85,3 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
-    });
-  });
